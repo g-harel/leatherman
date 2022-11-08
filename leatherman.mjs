aa13a34db278e80e70e9ea3fee7a45d100866ade
Gabriel Harel <gabrielj.harel@gmail.com>
2022-11-07 21:55:21 -0600

    rerun script

--- leatherman.mjs
+++ leatherman.mjs
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 
-const start = 0;
-const end = start + 715;
+const start = 695;
+const end = start + 30;
 
 for (let i = start; i < end; i++) {
   const url = `https://www.leatherman.com/${i}.html`;
