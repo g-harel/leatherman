2e9e44aa94c62aa6cde7ed952553dbbcd506f11e
Gabriel Harel <gabrielj.harel@gmail.com>
2023-02-03 23:39:38 -0600

    write output to file

--- leatherman.mjs
+++ leatherman.mjs
@@ -1,18 +1,25 @@
 import fetch from "node-fetch";
+import fs from "fs/promises";
+import path from "path";
 
-const start = 0;
-const end = start + 710;
-
-for (let i = start; i < end; i++) {
-  const url = `https://www.leatherman.com/${i}.html`;
-  const res = await fetch(url, { redirect: "manual" });
-  if (res.status === 301) {
-    let location = res.headers.get("location");
-    if (location.startsWith("/")) {
-      location = "https://www.leatherman.com" + location;
+const fetchIDs = async (start = 0, count = 1) => {
+  let out = "";
+  for (let i = start; i < start + count; i++) {
+    const url = `https://www.leatherman.com/${i}.html`;
+    const res = await fetch(url, { redirect: "manual" });
+    if (res.status === 301) {
+      let location = res.headers.get("location");
+      if (location.startsWith("/")) {
+        location = "https://www.leatherman.com" + location;
+      }
+      out += `${i} ${location}\n`;
+    } else {
+      out += `${i}\n`;
     }
-    console.log(i, location);
-  } else {
-    console.log(i);
+    process.stdout.write(".");
   }
+  return out;
 }
+
+const date = new Date().toISOString().substring(0, 10);
+fs.writeFile(path.join(".", "archive", date), await fetchIDs(685, 20));
