78761fa129e50f2a8016447d9f24bf63f028b936
Gabriel Harel <gabrielj.harel@gmail.com>
2023-02-05 22:58:43 -0600

    convert source to deno

--- leatherman.ts
+++ leatherman.ts
@@ -1,6 +1,7 @@
-import fetch from "node-fetch";
-import fs from "fs/promises";
-import path from "path";
+import {join} from "https://deno.land/std@0.176.0/path/mod.ts";
+import {printf} from "https://deno.land/std@0.176.0/fmt/printf.ts";
+
+const outDir = "archive";
 
 const fetchIDs = async (start = 0, count = 1) => {
   let out = "";
@@ -9,17 +10,17 @@ const fetchIDs = async (start = 0, count = 1) => {
     const res = await fetch(url, { redirect: "manual" });
     if (res.status === 301) {
       let location = res.headers.get("location");
-      if (location.startsWith("/")) {
+      if (location && location.startsWith("/")) {
         location = "https://www.leatherman.com" + location;
       }
       out += `${i} ${location}\n`;
     } else {
       out += `${i}\n`;
     }
-    process.stdout.write(".");
+    printf(".");
   }
   return out;
-}
+};
 
 const date = new Date().toISOString().substring(0, 10);
-fs.writeFile(path.join(".", "archive", date), await fetchIDs(685, 20));
+Deno.writeTextFile(join(".", outDir, date), await fetchIDs(685, 20));
